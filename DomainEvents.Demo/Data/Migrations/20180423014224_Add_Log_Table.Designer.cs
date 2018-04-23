5ef123b5a324ce5a74cb2a40da15986592992b29
Michel Eissa <michel.eissa85@gmail.com>
2018-04-22 20:44:48 -0500

    Add Logs table migration

--- DomainEvents.Demo/Data/Migrations/20180423014224_Add_Log_Table.Designer.cs
+++ DomainEvents.Demo/Data/Migrations/20180423014224_Add_Log_Table.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DomainEvents.Demo.Data.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180423014224_Add_Log_Table")]
+    partial class Add_Log_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DomainEvents.Demo/Data/Migrations/20180423014224_Add_Log_Table.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DomainEvents.Demo.Data.Migrations
+{
+    public partial class Add_Log_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Level = table.Column<int>(nullable: false),
+                    Message = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+        }
+    }
+}
