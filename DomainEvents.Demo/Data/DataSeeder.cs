d7218121ca1dd14991f091dd3cd094ba8305cbcf
Michel Eissa <michel.eissa85@gmail.com>
2018-04-22 21:53:28 -0500

    Make sure Seeding happens correctly

--- DomainEvents.Demo/Data/DataSeeder.cs
+++ DomainEvents.Demo/Data/DataSeeder.cs
@@ -9,26 +9,29 @@ namespace DomainEvents.Demo.Data
 {
     public class DataSeeder
     {
-    private readonly MyDbContext _context;
+        private readonly MyDbContext _context;
 
-    public DataSeeder(MyDbContext context)
+        public DataSeeder(MyDbContext context)
         {
-        _context = context;
+            _context = context;
         }
 
-    public async Task Seed()
+        public async Task Seed()
         {
-        _context.Database.EnsureCreated();
-        var emp = new Employee
+            _context.Database.EnsureCreated();
+            if (!_context.Employees.Any())
             {
-            FirstName = "John",
-            LastName = "Smith",
-            Ssn = "111-11-1111",
-            DateOfBirth = new DateTime(1980, 2, 1),
-            Address = "123 Main St."
-            };
-        _context.Employees.Add(emp);
-        await _context.SaveChangesAsync();
+                var emp = new Employee
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Ssn = "111-11-1111",
+                    DateOfBirth = new DateTime(1980, 2, 1),
+                    Address = "123 Main St."
+                };
+                _context.Employees.Add(emp);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
